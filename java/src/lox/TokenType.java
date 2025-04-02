b35cb91cbcda7201d8f95bb66b67a21fd9faa6e2
Otto Sellerstam <otto.sellerstam@analyzera.com>
2025-04-02 21:32:23 +0200

    Started work on Java AST interpreter

---
+++ java/src/lox/TokenType.java
@@ -0,0 +1,23 @@
+package lox;
+
+enum TokenType {
+    // Single character tokens.
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // One or two character tokens.
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // Literals.
+    IDENTIFIER, STRING, NUMBER,
+
+    // KEYWORDS,
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRNT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
+
