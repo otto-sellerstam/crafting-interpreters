d32246d91a7db58ece258cfcfb9db75e221ced0a
Otto Sellerstam <otto.sellerstam@analyzera.com>
2025-04-04 16:21:11 +0200

    Non-working version of GenerateAst to support Visitor

--- java/src/lox/Expr.java
+++ java/src/lox/Expr.java
@@ -3,41 +3,69 @@ package lox;
 import java.util.List;
 
 abstract class Expr {
-    static class Binary extends Expr {
-        final Expr left;
-        final Token operator;
-        final Expr right;
+    interface Visitor<R> {
+    R visitBinaryExpr(Binary expr);
+  }
+    R visitGroupingExpr(Grouping expr);
+  }
+    R visitLiteralExpr(Literal expr);
+  }
+    R visitUnaryExpr(Unary expr);
+  }
+ static class Binary extends Expr {
+ Binary(Expr left, Token operator, Expr right) {
+ this.left = left;
+ this.operator = operator;
+ this.right = right;
+ }
 
-        Binary(Expr left, Token operator, Expr right) {
-            this.left = left;
-            this.operator = operator;
-            this.right = right;
-        }
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitBinaryExpr(this)
     }
 
-    static class Grouping extends Expr {
-        final Expr expression;
+ final Expr left;
+ final Token operator;
+ final Expr right;
+ }
+ static class Grouping extends Expr {
+ Grouping(Expr expression) {
+ this.expression = expression;
+ }
 
-        Grouping(Expr expression) {
-            this.expression = expression;
-        }
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitGroupingExpr(this)
     }
 
-    static class Literal extends Expr {
-        final Object value;
+ final Expr expression;
+ }
+ static class Literal extends Expr {
+ Literal(Object value) {
+ this.value = value;
+ }
 
-        Literal(Object value) {
-            this.value = value;
-        }
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitLiteralExpr(this)
     }
 
-    static class Unary extends Expr {
-        final Token operator;
-        final Expr right;
+ final Object value;
+ }
+ static class Unary extends Expr {
+ Unary(Token operator, Expr right) {
+ this.operator = operator;
+ this.right = right;
+ }
 
-        Unary(Token operator, Expr right) {
-            this.operator = operator;
-            this.right = right;
-        }
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitUnaryExpr(this)
     }
+
+ final Token operator;
+ final Expr right;
+ }
+
+  abstract <R> R accept(Visitor<R> visitor);
 }
